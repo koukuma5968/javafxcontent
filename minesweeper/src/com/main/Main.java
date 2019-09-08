5388a84e3304f840b5f32c5a4301c04435ff8023
user <user@192.168.2.112>
2019-09-08 16:14:09 +0900

    new create

---
+++ minesweeper/src/com/main/Main.java
@@ -0,0 +1,13 @@
+package com.main;
+
+import com.view.stage.MineMainStage;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		MineMainStage.launch(MineMainStage.class, args);
+
+	}
+
+}
---
+++ minesweeper/src/com/view/stage/MineMainStage.java
@@ -0,0 +1,14 @@
+package com.view.stage;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MineMainStage extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
